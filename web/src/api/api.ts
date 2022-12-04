2869fdb168050393e5d69b677bdb6adcaffeccea
KoaLaYT <hytohyeah@outlook.com>
2022-12-04 20:48:31 +0800

    feat(web): use domain name instead of ip

--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -10,7 +10,7 @@ interface CommonResponse<T> {
 export class RestApi {
 
     static baseUrl = CommonUtil.isDev()
-        ? `http://localhost:3000` : 'http://47.100.225.7:3000'
+        ? `http://localhost:3000` : 'http://api.amentechsj.info/api'
 
     static async get<T>(path: string): Promise<T | undefined> {
         return this.safeFetch(path)
