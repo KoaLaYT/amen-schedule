5d65e2d3e7a6e5e7eaf96404dc70274cc7e287e4
KoaLaYT <hytohyeah@outlook.com>
2023-01-08 19:33:30 +0800

    feat(web): calculate lesson fee by month

--- web/src/api/stat.api.ts
+++ web/src/api/stat.api.ts
@@ -1,11 +1,15 @@
 import { RestApi } from "./api";
 
 export interface StatVo {
-    id: number
-    taughtDate: string
-    startTime: string
-    endTime: string
-    fee: number
+    id: number;
+    taughtDate: string;
+    startTime: string;
+    endTime: string;
+    fee: number;
+}
+
+export interface StatAllVo {
+    fee: number;
 }
 
 export class StatApi {
@@ -13,6 +17,18 @@ export class StatApi {
      * 获取某学生，在一段时间内上过的课.
      */
     static async summary(studentId: number, dateRange: [string, string]) {
-        return await RestApi.get<StatVo[]>(`/stat/${studentId}?startDate=${dateRange[0]}&endDate=${dateRange[1]}`) ?? []
+        return (
+            (await RestApi.get<StatVo[]>(
+                `/stat/${studentId}?startDate=${dateRange[0]}&endDate=${dateRange[1]}`
+            )) ?? []
+        );
+    }
+
+    /**
+     * 计算某月份的课时费.
+     */
+    static async all(month: string): Promise<number> {
+        const result = await RestApi.get<StatAllVo>(`/stat/all?month=${month}`);
+        return result?.fee || 0;
     }
 }
---
+++ web/src/components/AmsMonthFee.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="ams-month-fee">
+        <div class="ams-month-fee__title">算账咯</div>
+        <van-cell-group inset style="flex: 1">
+            <van-field
+                :model-value="monthStr"
+                is-link
+                readonly
+                name="month"
+                label="月份"
+                @click="monthPickerShow = true"
+            />
+            <van-popup
+                v-model:show="monthPickerShow"
+                round
+                position="bottom"
+                :style="{ height: '40%' }"
+            >
+                <van-datetime-picker
+                    v-model="month"
+                    type="year-month"
+                    title="选择月份"
+                    @confirm="onMonthConfirm"
+                    @cancel="monthPickerShow = false"
+                />
+            </van-popup>
+            <van-field :model-value="fee" readonly name="fee" label="课时费" />
+        </van-cell-group>
+        <div class="ams-month-fee__btn">
+            <van-button
+                round
+                block
+                type="primary"
+                @click="onSubmit"
+                :loading="loading"
+            >
+                确认
+            </van-button>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from "vue";
+import dayjs from "dayjs";
+import { StatApi } from "../api/stat.api";
+
+const loading = ref(false);
+const monthPickerShow = ref(false);
+
+const month = ref(new Date());
+const monthStr = computed(() => dayjs(month.value).format("YYYY-MM"));
+const fee = ref("");
+
+const onMonthConfirm = (value: Date) => {
+    month.value = value;
+    monthPickerShow.value = false;
+};
+
+const onSubmit = async () => {
+    loading.value = true;
+    const feeNum = await StatApi.all(monthStr.value).finally(() => {
+        loading.value = false;
+    });
+    fee.value = feeNum > 0 ? `¥${feeNum}` : "一分没有";
+};
+</script>
+
+<style scoped lang="scss">
+.ams-month-fee {
+    display: flex;
+    flex-flow: column;
+    height: 100%;
+
+    &__title {
+        color: var(--van-primary-color);
+        font-size: 1.2rem;
+        margin: 1rem auto;
+        text-align: center;
+    }
+    &__btn {
+        margin: 1rem;
+        justify-self: end;
+    }
+}
+</style>
